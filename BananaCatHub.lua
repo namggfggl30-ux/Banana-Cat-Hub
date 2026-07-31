1274ff8ed7f2cad24be6a15dbbc678224bcc7888
namggfggl30 <namggfggl30@gmail.com>
2026-07-31 17:27:49 +0700

    Update BananaCatHub.lua

--- BananaCatHub.lua
+++ BananaCatHub.lua
@@ -28,7 +28,7 @@ local menu = Instance.new("Frame")
 menu.Parent = gui
 menu.AnchorPoint = Vector2.new(0.5,0.5)
 menu.Position = UDim2.new(0.5,0,0.5,0)
-menu.Size = UDim2.new(0,240,0,320)
+menu.Size = UDim2.new(0,220,0,260)
 menu.BackgroundColor3 = black
 menu.Visible = false
 
@@ -198,7 +198,7 @@ for i = 1,5 do
 	btn.Parent = menu
 	btn.Size = UDim2.new(0,180,0,34)
     btn.AnchorPoint = Vector2.new(0.5,0)
-    btn.Position = UDim2.new(0.5,0,0,45+(i-1)*40)
+    btn.Position = UDim2.new(0.5,0,0,40+(i-1)*38)
 	btn.BackgroundColor3 = Color3.fromRGB(20,20,20)
 	btn.BorderSizePixel = 0
 	btn.Text = ScriptNames[id]
@@ -282,7 +282,7 @@ footer.Parent = menu
 
 footer.AnchorPoint = Vector2.new(0.5,1)
 footer.Size = UDim2.new(0,170,0,25)
-footer.Position = UDim2.new(0.5,0,1,-5)
+footer.Position = UDim2.new(0.5,0,1,-10)
 
 footer.BackgroundTransparency = 1
 footer.Text = "★ Banana Hub - Enjoy! ★"
