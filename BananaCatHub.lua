2ad40dd0c32e9da8e79e973f652e8b02e37aa445
namggfggl30 <namggfggl30@gmail.com>
2026-07-31 18:33:57 +0700

    Update BananaCatHub.lua

--- BananaCatHub.lua
+++ BananaCatHub.lua
@@ -176,7 +176,7 @@ for i = 1,5 do
 
     btn.TextColor3 = yellow
     btn.Font = Enum.Font.GothamSemibold
-    btn.TextSize = 14
+    btn.TextSize = 13
     btn.TextScaled = false
     btn.TextWrapped = false
     btn.ClipsDescendants = true
