0e12558647b869dbea23f03ff6dec06fbc35547b
namggfggl30 <namggfggl30@gmail.com>
2026-07-31 10:22:38 +0700

    Update BananaCatHub.lua

--- BananaCatHub.lua
+++ BananaCatHub.lua
@@ -210,8 +210,8 @@ btn.TextStrokeTransparency = 1
 
 local textLimit = Instance.new("UITextSizeConstraint")
 textLimit.Parent = btn
-textLimit.MaxTextSize = 22
-textLimit.MinTextSize = 16
+textLimit.MaxTextSize = 20
+textLimit.MinTextSize = 10
 
 	btn.AutoButtonColor = false
 
